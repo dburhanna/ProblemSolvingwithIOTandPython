378891b35adb77204c11364d49bf1f7c732ee399
dburhanna <58218477+dburhanna@users.noreply.github.com>
2021-02-12 12:44:31 -0500

    Update turtle_example.py

--- mu_code/Python 3 examples/turtle_example.py
+++ mu_code/Python 3 examples/turtle_example.py
@@ -1,16 +1,10 @@
 # Write your code here :-)
 import turtle as t
 
+t.pensize(25)
+t.color("red")
 for i in range(4):
     t.forward(200)
     t.right(90)
 
 print("Hello on a snowy day!")
-
-dog = ("Finn", "fat", 8)
-cat = ["Skipper", "fast",6]
-bird = [dog, cat]
-bird[1][1]= 54
-cat[2] = "slow"
-dog[2] = "cheesey"
-print(bird)
