85e1e9ca939ab32927514b6a8ffcd5e12808ed05
dburhanna <58218477+dburhanna@users.noreply.github.com>
2021-03-02 15:53:49 -0500

    Create BLE LAB IDEAS.py

---
+++ mu_code/python BLE desktop/BLE LAB IDEAS.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+# BLE ideas
+#1 - connect to phone and use it to turn on/off LED using app buttons
+#    - BLE background, client/server, central/peripheral, packets
+#2 - connect to phone as UART and receive data from step counter
+#3 - stream data from another sensor and plot on phone?
+#4 - connect to computer and make custom UART protocol(?) to send data from another sensor,
+#     store data to a file or plot
