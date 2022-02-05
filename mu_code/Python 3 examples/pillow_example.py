963bcd3057b784e37296de99ad6d8ca7f50beab3
Douglas Burhanna <Burhanna.Douglas@hermits.com>
2022-02-05 08:28:42 -0500

    code work

--- mu_code/Python 3 examples/pillow_example.py
+++ mu_code/Python 3 examples/pillow_example.py
@@ -3,7 +3,7 @@ import sys
 
 #use try and except to open a picture file
 try:
-    picture = Image.open("images/peru.jpeg")
+    picture = Image.open("snow.jpeg")
     print("File is open...\n")
 except:
     print("Unable to load image")
@@ -15,18 +15,18 @@ print(height)
 # using Image.Draw to get picture ready to draw on
 idraw = ImageDraw.Draw(picture)
 # text to draw on picture
-text = 'cheeseburgers are yummy'
+text = 'snow sux'
 print(text)
 print()
 # choose a font to draw on picture
-font = ImageFont.truetype("fonts/404ERROR.TTF", size=100)
+font = ImageFont.truetype("404ERROR.TTF", size=100)
 # add the text to the picture
 print("adding text to image...\n")
-idraw.text((200, 500), text,(149,165,166), font = font)
+idraw.text((200, 500), text,(255,165,166), font = font)
 
 #save the modified file with a new name AND file type
 print("saving file now...\n")
-picture.save('images/peru_watermarked.png')
+picture.save('peru_watermarked.png')
 #adding a comment at the END of the file!!!
 #adding a comment at the END of the file!!!
 #adding a comment at the END of the file!!!
@@ -35,4 +35,6 @@ picture.save('images/peru_watermarked.png')
 #adding a comment at the END of the file!!!
 #adding a comment at the END of the file!!!
 #adding a comment at the END of the file!!!
+#adding a comment at the END of the file!!!
+
 #adding a comment at the END of the file!!!
